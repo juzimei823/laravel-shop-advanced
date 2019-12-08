bc9b14a71b086fe8bb81b97a9ec8d1fdf2571143
vagrant <vagrant@homestead>
2019-12-08 07:12:17 +0000

    删除了package.json里的font

--- server.php
+++ server.php
@@ -17,5 +17,5 @@ $uri = urldecode(
 if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
     return false;
 }
-
+ // "@fortawesome/fontawesome-free": "^5.8.1",
 require_once __DIR__.'/public/index.php';
